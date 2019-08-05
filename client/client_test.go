092db04e0d59f7c1455d40ad8ef54657e722f607
Alex <alext@rtradetechnologies.com>
2019-08-05 15:33:38 -0700

    client: add some more helpers

--- client/client_test.go
+++ client/client_test.go
@@ -7,7 +7,6 @@ import (
 var (
 	api     = "https://api.compound.finance/api/v2"
 	account = "0xe18999d3f7e1a84e35bf9c15699b79e46eb44704"
-	cdai    = "0xf5dce57282a584d2746faf1593d3121fcac444dc"
 )
 
 func Test_Client(t *testing.T) {
@@ -18,7 +17,7 @@ func Test_Client(t *testing.T) {
 	if _, err := client.GetAccounts(); err != nil {
 		t.Fatal(err)
 	}
-	if _, err := client.GetCToken(cdai); err != nil {
+	if _, err := client.GetCToken(CompoundDAI.String()); err != nil {
 		t.Fatal(err)
 	}
 	if _, err := client.GetCTokens(); err != nil {
--- client/utils.go
+++ client/utils.go
@@ -5,6 +5,19 @@ import (
 	"net/http"
 )
 
+
+// Address is a compound contract address type
+type Address string
+
+func (a Address) String() string {
+	return string(a)
+}
+
+const (
+	// CompoundDAI is the address of the cDAI contract
+	CompoundDAI Address = Address("0xf5dce57282a584d2746faf1593d3121fcac444dc")
+)
+
 // sendRequest is used to send a request, and return the given body bytes
 func (c *Client) sendRequest(url string) ([]byte, error) {
 	request, err := http.NewRequest("GET", url, nil)
